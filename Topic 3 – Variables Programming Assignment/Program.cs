f57ee34393c46da2c1d0bb6845497a4f5e2c2be3
Aron John Bautista <701035287@GoTVDSB.ca>
2025-02-07 14:41:19 -0500

    Add project files.

---
+++ Topic 3 – Variables Programming Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Topic_3___Variables_Programming_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aron John E. Bautista Topic 3 - Variables Programming Assistant
+            Console.WriteLine("Part 1");
+            string course;
+
+            int myRoom = 29;
+            double price = 2.99;
+            course = "Computer Science!";
+            //These are the things i need or these things are the given
+            Console.WriteLine("This is room #" + myRoom);
+            Console.WriteLine("The price is $" + price);
+            Console.WriteLine("I am interested in " + course);
+            Console.WriteLine("");
+            //Part 1 done
+            Console.WriteLine("Part 2");
+            string name, graduationyear;
+
+            name = "Aron John E. Bautista";
+            graduationyear = "2027";
+            Console.WriteLine("My name is " + name + " and I'll graduate in " + graduationyear + ".");
+            Console.WriteLine("");
+            //Part 2 done
+        }
+    }
+}
